3e2afe1f006d918ee02a5df69c0f2dbedd944bc3
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2023-11-30 12:37:28 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+
+All notable changes to `php-filename-sanitize` will be documented in this file.
---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/src',
+        // __DIR__.'/tests',
+    ]);
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_82,
+        SetList::CODE_QUALITY,
+        SetList::EARLY_RETURN,
+        // SetList::TYPE_DECLARATION_STRICT,
+    ]);
+};
